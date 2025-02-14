46446327de22e246f5c126d48068301880ec020f
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-02-13 22:31:58 -0700

    c

---
+++ maze.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+import numpy as np
+
+
+class Maze(gym.Env):
+    metadata = {'render.modes': ['human']}
+    def __init__(self):
+        pass
+
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
+        pass
+
+    def _get_obs(self):
+        pass
